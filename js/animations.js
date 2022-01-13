e2654331f09e1209f4c7314186bf3f0712629570
Tomasz <snakerrek@gmail.com>
2022-01-13 21:30:34 +0100

    small changes in styles and usage of jQuery

---
+++ js/animations.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  if (window.jQuery) {
+    $("#home").fadeIn(2500);
+    $("#counter").fadeIn(3500);
+    $("#about").fadeIn(4500);
+    $("#explore-food").fadeIn(5500);
+    $("#testimonial").fadeIn(6500);
+    $("#faq").fadeIn(7500);
+    $("#footer").fadeIn(8500);
+  }
+};
--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-let nav = document.querySelector(".navigation-wrap");
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
-    nav.classList.add("scroll-on");
+    $(".navigation-wrap").addClass("scroll-on");
   } else {
-    nav.classList.remove("scroll-on");
+    $(".navigation-wrap").removeClass("scroll-on");
   }
 };
 
